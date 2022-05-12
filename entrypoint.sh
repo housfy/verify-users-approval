a318bea5f3fffb9a67b1f93c6f0673398e8bd96d
Jesus Ortega <jesus.ortega@housfy.com>
2022-05-12 09:15:28 +0200

    New approach

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,45 @@
 #!/bin/sh -l
 
 # Define variables
-USERS=$1
-JSONCONTENT=$2
+OWNER=$1
+REPO=$2
+PULLNUMBER=$3
+USERS=$(echo $4 | sed -e 's/,/|/g')
+MINREVIEWERS=$5
+REVIEWERSJSON=''
 
-result=$( "$JSONCONTENT" > file.json | less file.json | jq)
+function getReviewers() {
+    #do curl get result
+    
+    REVIEWERSJSON=$(curl \
+  --fail \
+  --header "Accept: application/vnd.github.v3+json" \
+  --header "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$OWNER/$REPO/pulls/$PULLNUMBER/reviews)
 
-echo "Init 1" 
-echo "$result"
-echo "End 1"
+  if [ "$?" -eq 0 ]; then
+    echo "JSON Received"
+  else
+    echo "Failed to get json"
+    exit 1
+  fi
+}
+
+function parseApproves() {
+    result=$(echo $REVIEWERSJSON | jq '.[] |  select(.state == "APPROVED") | .user.login' | grep -cE $USERS)
+
+    if(( $result>=$MINREVIEWERS ))
+    then
+        printf "Reviwers accepted\n"
+        printf "Finishing all good."
+    else
+        printf "Minium required reviewers not met\n"
+        printf "Finishing with failed."
+        exit 1
+    fi
+}
+
+printf "Getting Reviewers \n"
+getReviewers
+printf "Parsing approvers \n"
+parseApproves
